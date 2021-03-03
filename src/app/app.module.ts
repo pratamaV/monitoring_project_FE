18e33e5dc7ef2999396ce6342a8805497e15e409
unknown <pratamaviadi@gmail.com>
2021-03-04 00:48:47 +0700

    [FEATURE] Make component auth, master, layout, and not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthComponent } from './auth/auth.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MasterComponent } from './master/master.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    AuthComponent,
+    LayoutComponent,
+    SidebarComponent,
+    MasterComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule
