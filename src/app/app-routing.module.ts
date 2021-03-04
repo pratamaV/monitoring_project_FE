b5fde6b52ceaf13378bbf887b24881c512eaef5f
unknown <pratamaviadi@gmail.com>
2021-03-04 15:36:09 +0700

    [FIXING] Fix error can show in home.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './master/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'auth', component: LoginComponent},
-  {path: '', loadChildren: () => import('./master/master.module').then((m) => m.MasterModule)},
+  {path: 'dashboard', loadChildren: () => import('./master/master.module').then((m) => m.MasterModule)},
   {path: 'logout', loadChildren: () => import('./auth/auth.module').then((a) => a.AuthModule)},
   {path: '**', component: NotFoundComponent}
   ];
 
 
-  @NgModule({
+@NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  
+
 export class AppRoutingModule { }
--- src/app/master/master-routing.module.ts
+++ src/app/master/master-routing.module.ts
@@ -3,7 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
   {path: 'user', loadChildren: () => import('./user/user.module').then((u) => u.UserModule)},
   {path: 'project', loadChildren: () => import('./project/project.module').then((p) => p.ProjectModule)},
   ];
