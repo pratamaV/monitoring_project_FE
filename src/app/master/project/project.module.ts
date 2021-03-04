b6dae6dfb2f49ed8120e5b40a77f55b1cb7428b0
unknown <pratamaviadi@gmail.com>
2021-03-04 17:20:38 +0700

    [IMPROVE] Can route when define first in module

--- src/app/master/project/project.module.ts
+++ src/app/master/project/project.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ListProjectComponent } from './list-project/list-project.component';
 import { FormProjectComponent } from './form-project/form-project.component';
 import { DetailProjectComponent } from './detail-project/detail-project.component';
+import {ProjectRoutingModule} from "./project-routing.module";
 
 
 
 @NgModule({
   declarations: [ListProjectComponent, FormProjectComponent, DetailProjectComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ProjectRoutingModule
   ]
 })
 export class ProjectModule { }
--- src/app/master/user/user.module.ts
+++ src/app/master/user/user.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
+
+
+import{ NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListUserComponent } from './list-user/list-user.component';
 import { FormUserComponent } from './form-user/form-user.component';
 import { DetailUserComponent } from './detail-user/detail-user.component';
-
-
+import {UserRoutingModule} from "./user-routing.module";
 
 @NgModule({
   declarations: [DetailUserComponent, FormUserComponent, ListUserComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+UserRoutingModule]
 })
 export class UserModule { }
