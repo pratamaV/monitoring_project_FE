296e55f66876a0843f15ef15274572d7534cc0fd
unknown <pratamaviadi@gmail.com>
2021-03-04 15:51:33 +0700

    [IMPROVE] Design dashboard

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $('[data-toggle="offcanvas"]').click(function(){
-        $("#navigation").toggleClass("hidden-xs");
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".side-nav .collapse").on("hide.bs.collapse", function() {
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
     });
- });
+    $('.side-nav .collapse').on("show.bs.collapse", function() {
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");
+    });
+})
