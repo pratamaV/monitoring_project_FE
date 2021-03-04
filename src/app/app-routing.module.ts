73a12e99a99442a1b0374b6ecff354dae19e9d57
unknown <pratamaviadi@gmail.com>
2021-03-04 16:08:45 +0700

    [IMPROVE]Make form and table in user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule }   from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './master/home/home.component';
