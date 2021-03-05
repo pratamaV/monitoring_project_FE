4f87d04356f49bdf4bbfdfd6d8f5c1c767c935fc
unknown <1309rizki@gmail.com>
2021-03-05 23:11:42 +0700

    [FEATURE] service release

---
+++ src/app/master/release/release.model.ts
@@ -0,0 +1,37 @@
+import {ProjectModel} from "../project/project.model";
+
+export interface ReleaseModel{
+  id: string;
+  releaseCode: string;
+  releaseName: string;
+  score: number;
+  weight: number;
+  description: string;
+  status: string;
+  stage: string;
+  prosentaseRelease: number;
+  estStartdate: Date;
+  estEnddate: Date;
+  actStartdate: Date;
+  actEnddate: Date;
+  project: {
+    id: string;
+  };
+}
+
+export interface ReleaseModel2{
+  id: string;
+  releaseCode: string;
+  releaseName: string;
+  score: number;
+  weight: number;
+  description: string;
+  status: string;
+  stage: string;
+  prosentaseRelease: number;
+  estStartdate: Date;
+  estEnddate: Date;
+  actStartdate: Date;
+  actEnddate: Date;
+  project: ProjectModel;
+}
