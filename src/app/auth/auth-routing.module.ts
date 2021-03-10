04c2ee8905cc132a68d89d955197f250ab471d36
mfadly28 <fadlymhd43@gmail.com>
2021-03-10 16:00:25 +0700

    [FEATURE] add login page

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import {LogoutComponent} from './logout/logout.component';
 
 
 const routes: Routes = [
-  {path: '', component: LogoutComponent}
+  {path: '', component: LogoutComponent},
+  // {path: 'login', component: LoginComponent}
+  
 ];
 
 
