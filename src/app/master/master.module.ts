a1521f7dc136c08a6e6e08408e974c01753f8bac
unknown <pratamaviadi@gmail.com>
2021-03-11 15:14:47 +0700

    [FIX] Merge from home

--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MasterRoutingModule} from './master-routing.module';
-import {MasterComponent} from './master.component';
-import {LayoutModule} from '../layout/layout.module';
-
+import { LayoutModule } from "../layout/layout.module";
 
 
 @NgModule({
-  declarations: [MasterComponent],
+  declarations: [],
   imports: [
     CommonModule,
     MasterRoutingModule,
--- src/app/master/user/user.module.ts
+++ src/app/master/user/user.module.ts
@@ -1,16 +1,16 @@
-
-
 import{ NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListUserComponent } from './list-user/list-user.component';
+import { ListUserComponent} from "./list-user/list-user.component";
 import { FormUserComponent } from './form-user/form-user.component';
-import { DetailUserComponent } from './detail-user/detail-user.component';
+import { DetailUserComponent} from "./detail-user/detail-user.component";
 import {UserRoutingModule} from "./user-routing.module";
+import {LayoutModule} from "../../layout/layout.module";
 
 @NgModule({
   declarations: [DetailUserComponent, FormUserComponent, ListUserComponent],
   imports: [
     CommonModule,
-UserRoutingModule]
+    UserRoutingModule,
+    LayoutModule]
 })
-export class UserModule { }
+export class UserModule{}
