fe8a50df2a8a22d831def2255b9c437c57cba848
unknown <1309rizki@gmail.com>
2021-03-15 09:41:02 +0700

    [IMPROVE] implement sidebar

--- src/app/master/issued/issued.module.ts
+++ src/app/master/issued/issued.module.ts
@@ -5,14 +5,16 @@ import { IssuedRoutingModule } from './issued-routing.module';
 import { FormIssuedComponent } from './form-issued/form-issued.component';
 import { ListIssuedComponent } from './list-issued/list-issued.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {SidebarModule} from "../../layout/sidebar/sidebar.module";
 
 
 @NgModule({
   declarations: [FormIssuedComponent, ListIssuedComponent],
-  imports: [
-    CommonModule,
-    IssuedRoutingModule,
-    ReactiveFormsModule
-  ]
+    imports: [
+        CommonModule,
+        IssuedRoutingModule,
+        ReactiveFormsModule,
+        SidebarModule
+    ]
 })
 export class IssuedModule { }
