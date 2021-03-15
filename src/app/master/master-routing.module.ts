9d730b1719e2b599b9634c46530a3ef2552d5638
andreas.wijaya <andreasputrawijaya1996@gmail.com>
2021-03-15 11:43:10 +0700

    [FEATURE] add authguard in login

--- src/app/master/master-routing.module.ts
+++ src/app/master/master-routing.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {TaskComponent} from './task/task.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from '../auth-guard.service';
+import { TaskComponent } from './task/task.component';
 
 const routes: Routes = [
-  {path: 'user', loadChildren: () => import('./user/user.module').then((u) => u.UserModule)},
-  {path: 'project', loadChildren: () => import('./project/project.module').then((p) => p.ProjectModule)},
-  {path: 'release', loadChildren: () => import('./release/release.module').then((p) => p.ReleaseModule)},
-  {path: 'task', loadChildren: () => import('./task/task.module').then((p) => p.TaskModule)},
-  {path: 'issued', loadChildren: () => import('./issued/issued.module').then((p) => p.IssuedModule)}
-  ];
-
+  {
+    path: 'user',
+    canActivate: [AuthGuardService],
+    loadChildren: () => import('./user/user.module').then(u => u.UserModule)
+  },
+  {
+    path: 'project',
+    canActivate: [AuthGuardService],
+    loadChildren: () =>
+      import('./project/project.module').then(p => p.ProjectModule)
+  },
+  {
+    path: 'release',
+    canActivate: [AuthGuardService],
+    loadChildren: () =>
+      import('./release/release.module').then(p => p.ReleaseModule)
+  },
+  {
+    path: 'task',
+    canActivate: [AuthGuardService],
+    loadChildren: () => import('./task/task.module').then(p => p.TaskModule)
+  },
+  {
+    path: 'issued',
+    canActivate: [AuthGuardService],
+    loadChildren: () =>
+      import('./issued/issued.module').then(p => p.IssuedModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MasterRoutingModule { }
+export class MasterRoutingModule {}
